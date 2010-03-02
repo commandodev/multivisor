095b58e3af6dc204e834e5bc078e76ee16d9a16d
Ben Ford <ben.fordnz@gmail.com>
2010-03-02 21:00:50 +0000

    Initial project structure

---
+++ README.txt
@@ -0,0 +1,4 @@
+multivisor README
+
+
+
---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = ['repoze.bfg']
+
+setup(name='multivisor',
+      version='0.0',
+      description='multivisor',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: BFG",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="multivisor",
+      entry_points = """\
+      [paste.app_factory]
+      app = multivisor.run:app
+      """
+      )
+
